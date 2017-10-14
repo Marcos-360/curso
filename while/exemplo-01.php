029058a234b62a0d0b6c0d8d718ec4ac388c55ca
Marcos <marcos@efigie.net>
2017-10-14 17:28:20 -0300

    while, aula 24

--- while/exemplo-01.php
+++ while/exemplo-01.php
@@ -1,1 +1,21 @@
 <?php
+
+$condicao = true;
+
+while ($condicao) {
+    
+    $numero = rand(1,15);
+    
+    if ($numero === 3) {
+        
+        echo "TRÊS! ";
+        $condicao = false;
+        
+    }
+    
+    echo $numero . " ";
+    
+}
+
+
+?>
